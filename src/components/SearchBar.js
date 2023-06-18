07bb86d6d515eb84f734373ea801a988690d16a2
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-06-18 16:23:31 +0200

    created searchbar component skeleton

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Button}  from 'react-native';
+
+function SearchBar(business, location) {
+  return (
+    <div>
+      <Button
+        title="Search"
+       // onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      SearchBar
+    </div>
+
+  )
+}
+
+export default SearchBar
